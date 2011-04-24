4d6733255d3e32f1a2ce7965a96fab4f8c7d0e52
jumar <jumar@61682b0c-635e-44ba-a85b-d27e7dc34134>
2011-04-24 08:40:15 +0000

    id generation strategy  switched to TABLE temporarily until DB schema is updated to reflect using IDENTITY strategy
    git-svn-id: https://svn.webgres.cz/svn/poptavka@318 61682b0c-635e-44ba-a85b-d27e7dc34134

--- poptavka-core/src/main/java/cz/poptavka/sample/domain/common/DomainObject.java
+++ poptavka-core/src/main/java/cz/poptavka/sample/domain/common/DomainObject.java
@@ -11,7 +11,7 @@ public abstract class DomainObject implements Serializable {
 
     /** Id of the entity. */
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.TABLE)
     private Long id;
 
 
