48030a539b43c147b86fd485175d8af139b65bd6
jumar <jumar@61682b0c-635e-44ba-a85b-d27e7dc34134>
2011-05-08 20:34:51 +0000

    package import for ResultCriteria fixed
    git-svn-id: https://svn.webgres.cz/svn/poptavka@396 61682b0c-635e-44ba-a85b-d27e7dc34134

--- poptavka-web/src/main/java/cz/poptavka/sample/client/service/demand/DemandRPCService.java
+++ poptavka-web/src/main/java/cz/poptavka/sample/client/service/demand/DemandRPCService.java
@@ -13,7 +13,7 @@ import java.util.Set;
 import com.google.gwt.user.client.rpc.RemoteService;
 import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
 
-import cz.poptavka.sample.domain.ResultCriteria;
+import cz.poptavka.sample.domain.common.ResultCriteria;
 import cz.poptavka.sample.domain.address.Locality;
 import cz.poptavka.sample.domain.demand.Category;
 import cz.poptavka.sample.domain.demand.Demand;
