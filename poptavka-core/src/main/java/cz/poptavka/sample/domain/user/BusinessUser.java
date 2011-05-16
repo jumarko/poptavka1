fd5fa833d2930fd5da52b5634bc1187f48dfa024
jumar <jumar@61682b0c-635e-44ba-a85b-d27e7dc34134>
2011-05-16 08:27:06 +0000

    Unused netsted enum Verification removed
    git-svn-id: https://svn.webgres.cz/svn/poptavka@425 61682b0c-635e-44ba-a85b-d27e7dc34134

--- poptavka-core/src/main/java/cz/poptavka/sample/domain/user/BusinessUser.java
+++ poptavka-core/src/main/java/cz/poptavka/sample/domain/user/BusinessUser.java
@@ -117,23 +117,6 @@ public class BusinessUser extends User {
 
     //-------------------------- End of GETTERS AND SETTERS ------------------------------------------------------------
 
-
-    public static enum Verification {
-        /** User has already been verified by email link activation. */
-        VERIFIED,
-
-        /**
-         * User has filled out our registration form and received email link activation.
-         * If user was in state EXTERNAL before registration we will change it to UNVERIFIED
-         * and we will be waiting for email link activation.
-         */
-        UNVERIFIED,
-
-        /** User came from external system and will be verified after email link activation is performed. */
-        EXTERNAL
-    }
-
-
     public String toString() {
         final StringBuilder sb = new StringBuilder();
         sb.append("BusinessUser");
