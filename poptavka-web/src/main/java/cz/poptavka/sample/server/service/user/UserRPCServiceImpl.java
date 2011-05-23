15b77f119fa0a9ef494661028e5595942a41af3d
praso <praso@61682b0c-635e-44ba-a85b-d27e7dc34134>
2011-05-23 17:29:37 +0000

    UserRPCServiceImpl loginUser
    git-svn-id: https://svn.webgres.cz/svn/poptavka@461 61682b0c-635e-44ba-a85b-d27e7dc34134

--- poptavka-web/src/main/java/cz/poptavka/sample/server/service/user/UserRPCServiceImpl.java
+++ poptavka-web/src/main/java/cz/poptavka/sample/server/service/user/UserRPCServiceImpl.java
@@ -1,11 +1,12 @@
 package cz.poptavka.sample.server.service.user;
 
-import org.springframework.beans.factory.annotation.Autowired;
-
+import com.googlecode.genericdao.search.Search;
 import cz.poptavka.sample.client.service.demand.UserRPCService;
+import cz.poptavka.sample.domain.user.User;
 import cz.poptavka.sample.server.service.AutoinjectingRemoteService;
 import cz.poptavka.sample.service.GeneralService;
 import cz.poptavka.sample.shared.domain.UserDetail;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class UserRPCServiceImpl extends AutoinjectingRemoteService implements UserRPCService {
 
@@ -19,13 +20,16 @@ public class UserRPCServiceImpl extends AutoinjectingRemoteService implements Us
     }
 
     @Override
-    public UserDetail loginUser(UserDetail user) {
-//        final List<User> existingUsers = this.generalService.searchByCriteria(
-//                UserSearchCriteria.Builder.userSearchCriteria()
-//                .withEmail(user.getEmail())
-//                .withPassword(user.getPassword())
-//                .build());
-        return null;
-    }
+    public UserDetail loginUser(UserDetail userDetail) {
 
+        final User user = (User) generalService.searchUnique(
+                new Search(User.class).addFilterEqual("email", userDetail.getEmail())
+                    .addFilterEqual("password", userDetail.getPassword()));
+        if (user == null) {
+            return null;
+        }
+        userDetail.setEmail(user.getEmail());
+        userDetail.setPassword(user.getPassword());
+        return userDetail;
+    }
 }
