d67f44f4d979b71faebf71f10b0efca4bb5a1c07
jumar <jumar@61682b0c-635e-44ba-a85b-d27e7dc34134>
2011-05-25 08:22:02 +0000

    cleanup - obsolete code removed, javadoc updated
    git-svn-id: https://svn.webgres.cz/svn/poptavka@476 61682b0c-635e-44ba-a85b-d27e7dc34134

--- poptavka-core/src/main/java/cz/poptavka/sample/domain/common/TreeItem.java
+++ poptavka-core/src/main/java/cz/poptavka/sample/domain/common/TreeItem.java
@@ -1,13 +1,11 @@
 package cz.poptavka.sample.domain.common;
 
 import javax.persistence.Column;
-import javax.persistence.Inheritance;
-import javax.persistence.InheritanceType;
 import javax.persistence.MappedSuperclass;
 
 /**
  * Base class that enables working with tree data structure. Objects that extends this class can be placed
- * withing n-ary trees.
+ * within n-ary trees.
  *
  * <p>
  * The class implements the optimization which uses <code>leftBound</code> and <code>rightBound</code>.
@@ -18,12 +16,13 @@ import javax.persistence.MappedSuperclass;
  * for more information.
  *
  * <p>
- *     The descendant of this class must override abstract methods and maps related fields. These fields cannot
- *     be mapped in this superclass because then the strange errors occurs - e.g. the method {@@link #getChildren()}
+ *     The descendant of this class must override abstract methods and maps related fields
+ *     (<code>children</code> and <code>parent</code>).
+ *     These fields cannot be mapped in this superclass because then the strange errors occurs
+ *     - e.g. the method <code>getChildren()</code>
  *     returns both categories and localities when called on Locality object.
  *
- * This class uses TABLE_PER_CLASS strategy because there is a references to the parent - this case is not allowed
- * to be presented in @MappedSuperclass.
+ * The case where there is a references to the parent is not allowed to be presented in @MappedSuperclass.
  *
  * See <a href="http://opensource.atlassian.com/projects/hibernate/browse/EJB-199">this jira discussion</a>
  * for further explanation.
@@ -31,10 +30,20 @@ import javax.persistence.MappedSuperclass;
  * @author Juraj Martinka
  *         Date: 31.1.11
  */
-//@Entity
-@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 @MappedSuperclass
 public abstract class TreeItem extends DomainObject {
+
+    /*
+     * Mapping of parent and children is not allowed in superclass - must be defined in descendant!
+    */
+
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    private TreeItem parent;
+//
+//    /** All children of this tree item in tree structure. */
+//    @OneToMany(mappedBy = "parent")
+//    private List<TreeItem> children;
+
 
     /** The depth from root TreeItem. */
     private int level;
@@ -72,15 +81,5 @@ public abstract class TreeItem extends DomainObject {
     public void setRightBound(Long rightBound) {
         this.rightBound = rightBound;
     }
-
-
-//    //--------------------- abstract methods to enforce implementation of problematic fields in subclass -------------
-//    public abstract T getParent();
-//
-//    public abstract void setParent(T parent);
-//
-//    public abstract List<T> getChildren();
-//
-//    public abstract void setChildren(List<T> children);
 
 }
