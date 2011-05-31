ddf684912c8766ed6077db23a47cf52ef593198a
praso <praso@61682b0c-635e-44ba-a85b-d27e7dc34134>
2011-05-31 12:22:52 +0000

    OfferDetail object
    git-svn-id: https://svn.webgres.cz/svn/poptavka@533 61682b0c-635e-44ba-a85b-d27e7dc34134

--- poptavka-web/src/main/java/cz/poptavka/sample/shared/domain/OfferDetail.java
+++ poptavka-web/src/main/java/cz/poptavka/sample/shared/domain/OfferDetail.java
@@ -10,60 +10,76 @@ public class OfferDetail implements Serializable {
      * Generated serialVersionUID.
      */
     private static final long serialVersionUID = -563380651738612866L;
-
     private BigDecimal price;
     private boolean displayed = false;
     private Date finishDate;
     private long demandId;
     private long supplierId;
-    private String supplierName;
     private long messageId;
+    private MessageDetail messageDetail;
 
     public OfferDetail() {
     }
 
-
     public BigDecimal getPrice() {
         return price;
     }
+
     public void setPrice(BigDecimal price) {
         this.price = price;
     }
+
     public boolean isDisplayed() {
         return displayed;
     }
+
     public void setDisplayed(boolean displayed) {
         this.displayed = displayed;
     }
+
     public Date getFinishDate() {
         return finishDate;
     }
+
     public void setFinishDate(Date finishDate) {
         this.finishDate = finishDate;
     }
+
     public long getDemandId() {
         return demandId;
     }
+
     public void setDemandId(long demandId) {
         this.demandId = demandId;
     }
+
     public long getSupplierId() {
         return supplierId;
     }
+
     public void setSupplierId(long supplierId) {
         this.supplierId = supplierId;
     }
-    public String getSupplierName() {
-        return supplierName;
-    }
-    public void setSupplierName(String supplierName) {
-        this.supplierName = supplierName;
-    }
+
     public void setMessageId(long id) {
         this.messageId = id;
     }
+
     public long getMessageId() {
         return messageId;
     }
 
+    /**
+     * @return the messageDetail
+     */
+    public MessageDetail getMessageDetail() {
+        return messageDetail;
+    }
+
+    /**
+     * @param messageDetail the messageDetail to set
+     */
+    public void setMessageDetail(MessageDetail messageDetail) {
+        this.messageDetail = messageDetail;
+    }
 }
