1c6deb404d87ddc3e5dd2d3009fa1eb334a9b213
praso <praso@61682b0c-635e-44ba-a85b-d27e7dc34134>
2011-06-24 10:44:34 +0000

    Update of Detail Object  definition for retrieving list of ClientDemandMessageDetail
    git-svn-id: https://svn.webgres.cz/svn/poptavka@585 61682b0c-635e-44ba-a85b-d27e7dc34134

--- poptavka-web/src/main/java/cz/poptavka/sample/shared/domain/message/ClientDemandMessageDetail.java
+++ poptavka-web/src/main/java/cz/poptavka/sample/shared/domain/message/ClientDemandMessageDetail.java
@@ -20,7 +20,162 @@ public class ClientDemandMessageDetail {
     private int unreadSubmessages;
     // demand fields
     private String demandTitle;
+    private String demandStatus;
     private Date endDate;
     private Date expiryDate;
     private BigDecimal price;
+
+    /**
+     * @return the messageId
+     */
+    public long getMessageId() {
+        return messageId;
+    }
+
+    /**
+     * @param messageId the messageId to set
+     */
+    public void setMessageId(long messageId) {
+        this.messageId = messageId;
+    }
+
+    /**
+     * @return the threadRoodId
+     */
+    public long getThreadRoodId() {
+        return threadRoodId;
+    }
+
+    /**
+     * @param threadRoodId the threadRoodId to set
+     */
+    public void setThreadRoodId(long threadRoodId) {
+        this.threadRoodId = threadRoodId;
+    }
+
+    /**
+     * @return the demandId
+     */
+    public long getDemandId() {
+        return demandId;
+    }
+
+    /**
+     * @param demandId the demandId to set
+     */
+    public void setDemandId(long demandId) {
+        this.demandId = demandId;
+    }
+
+    /**
+     * @return the senderId
+     */
+    public long getSenderId() {
+        return senderId;
+    }
+
+    /**
+     * @param senderId the senderId to set
+     */
+    public void setSenderId(long senderId) {
+        this.senderId = senderId;
+    }
+
+    /**
+     * @return the unreadSubmessages
+     */
+    public int getUnreadSubmessages() {
+        return unreadSubmessages;
+    }
+
+    /**
+     * @param unreadSubmessages the unreadSubmessages to set
+     */
+    public void setUnreadSubmessages(int unreadSubmessages) {
+        this.unreadSubmessages = unreadSubmessages;
+    }
+
+    /**
+     * @return the demandTitle
+     */
+    public String getDemandTitle() {
+        return demandTitle;
+    }
+
+    /**
+     * @param demandTitle the demandTitle to set
+     */
+    public void setDemandTitle(String demandTitle) {
+        this.demandTitle = demandTitle;
+    }
+
+    /**
+     * @return the demandStatus
+     */
+    public String getDemandStatus() {
+        return demandStatus;
+    }
+
+    /**
+     * @param demandStatus the demandStatus to set
+     */
+    public void setDemandStatus(String demandStatus) {
+        this.demandStatus = demandStatus;
+    }
+
+    /**
+     * @return the endDate
+     */
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    /**
+     * @param endDate the endDate to set
+     */
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    /**
+     * @return the expiryDate
+     */
+    public Date getExpiryDate() {
+        return expiryDate;
+    }
+
+    /**
+     * @param expiryDate the expiryDate to set
+     */
+    public void setExpiryDate(Date expiryDate) {
+        this.expiryDate = expiryDate;
+    }
+
+    /**
+     * @return the price
+     */
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    /**
+     * @param price the price to set
+     */
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "ClientDemandMessageDetail{" + "messageId=" + messageId
+                + ", threadRoodId=" + threadRoodId
+                + ", demandId=" + demandId
+                + ", senderId=" + senderId
+                + ", unreadSubmessages=" + unreadSubmessages
+                + ", demandTitle=" + demandTitle
+                + ", demandStatus=" + demandStatus
+                + ", endDate=" + endDate
+                + ", expiryDate=" + expiryDate
+                + ", price=" + price + '}';
+    }
 }
