0d1c8742f9fa46b2c78d6844604e1b56f071f09d
jumar <jumar@61682b0c-635e-44ba-a85b-d27e7dc34134>
2011-10-06 16:38:16 +0000

    new test added for (mainly) demonstration of GeneralService#search method
    git-svn-id: https://svn.webgres.cz/svn/poptavka@742 61682b0c-635e-44ba-a85b-d27e7dc34134

--- poptavka-core/src/test/java/cz/poptavka/sample/service/GeneralServiceIntegrationTest.java
+++ poptavka-core/src/test/java/cz/poptavka/sample/service/GeneralServiceIntegrationTest.java
@@ -1,8 +1,10 @@
 package cz.poptavka.sample.service;
 
+import com.googlecode.genericdao.search.Search;
 import cz.poptavka.sample.base.integration.DBUnitBaseTest;
 import cz.poptavka.sample.base.integration.DataSet;
 import cz.poptavka.sample.domain.address.Locality;
+import org.hamcrest.core.Is;
 import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,5 +32,28 @@ public class GeneralServiceIntegrationTest extends DBUnitBaseTest {
         final List<Locality> allLocalities = this.generalService.findAll(Locality.class);
 
         Assert.assertEquals("All localities must be found - nothing less nothing more.", 12, allLocalities.size());
+    }
+
+    @Test
+    public void testSearchLocalities() {
+        final Search localitySearch = new Search(Locality.class);
+        localitySearch.setFirstResult(0);
+        localitySearch.setMaxResults(4);
+        localitySearch.addSortAsc("name");
+
+        final List<Locality> localitySearchResult = this.generalService.search(localitySearch);
+        Assert.assertNotNull(localitySearchResult);
+        Assert.assertThat("Incorrect size of locality search result", localitySearchResult.size(), Is.is(4));
+
+        // check if localities appear in expected order
+        checkLocality(localitySearchResult, "Czech Republic", 0);
+        checkLocality(localitySearchResult, "locality1", 1);
+        checkLocality(localitySearchResult, "locality11", 2);
+        checkLocality(localitySearchResult, "locality111", 3);
+    }
+
+    private void checkLocality(List<Locality> localitySearchResult, String expectedLocalityName, int localityIndex) {
+        Assert.assertThat("Incorrect locality",
+                localitySearchResult.get(localityIndex).getName(), Is.is(expectedLocalityName));
     }
 }
