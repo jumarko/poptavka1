b54875fe7734dd5d290e60fc76e0ea770433022b
jumar <jumar@61682b0c-635e-44ba-a85b-d27e7dc34134>
2012-02-19 09:31:21 +0000

    Test class ignored temporarily because it does not contain any tests.
    git-svn-id: https://svn.webgres.cz/svn/poptavka@872 61682b0c-635e-44ba-a85b-d27e7dc34134

--- poptavka-rest-api/src/test/java/cz/poptavka/sample/rest/common/AbstractPageableResourceTest.java
+++ poptavka-rest-api/src/test/java/cz/poptavka/sample/rest/common/AbstractPageableResourceTest.java
@@ -3,10 +3,13 @@
  */
 package cz.poptavka.sample.rest.common;
 
+import org.junit.Ignore;
+
 /**
  * For test of {@link cz.poptavka.sample.rest.common.resource.AbstractPageableResource#listPage(String, int, int)}
  * method see {@link cz.poptavka.sample.rest.demand.DemandResourceTest}.
  */
+@Ignore // no tests for now, therefore ignored
 public class AbstractPageableResourceTest {
 
 }
