1e47268282aac1544ecaeea058669b280ea2316a
kolki <kolki@61682b0c-635e-44ba-a85b-d27e7dc34134>
2012-03-21 18:28:33 +0000

    added basic support for spring security authentication
    git-svn-id: https://svn.webgres.cz/svn/poptavka@917 61682b0c-635e-44ba-a85b-d27e7dc34134

--- poptavka-web/src/main/java/cz/poptavka/sample/client/main/login/LoginPopupPresenter.java
+++ poptavka-web/src/main/java/cz/poptavka/sample/client/main/login/LoginPopupPresenter.java
@@ -3,6 +3,13 @@ package cz.poptavka.sample.client.main.login;
 import java.util.Date;
 import java.util.logging.Logger;
 
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.http.client.Request;
+import com.google.gwt.http.client.RequestBuilder;
+import com.google.gwt.http.client.RequestCallback;
+import com.google.gwt.http.client.RequestException;
+import com.google.gwt.http.client.Response;
+import com.google.gwt.http.client.URL;
 import com.google.gwt.user.client.Cookies;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.inject.Inject;
@@ -55,6 +62,37 @@ public class LoginPopupPresenter extends LazyPresenter<LoginPopupPresenter.Login
             view.setLoadingStatus();
             String username = view.getLogin();
             String password = view.getPassword();
+            RequestBuilder builder = new RequestBuilder(RequestBuilder.POST,
+                    URL.encode("/poptavka/j_spring_security_check"));
+            builder.setHeader("Content-type", "application/x-www-form-urlencoded");
+            try {
+                Request request = builder.sendRequest("j_username=" + view.getLogin()
+                        + "&j_password=" + view.getPassword(),
+                        new RequestCallback() {
+                            public void onError(Request request,
+                                    Throwable exception) {
+                                // Couldn't connect to server (could be timeout,
+                                // SOP violation, etc.)
+                                GWT.log("error during login");
+                            }
+
+                            public void onResponseReceived(Request request,
+                                    Response response) {
+                                if (200 == response.getStatusCode()) {
+                                    // Process the response in
+                                    GWT.log(response.getText());
+                                } else {
+                                    // Handle the error. Can get the status text
+                                    // from response.getStatusText()
+                                    view.setLoginError();
+                                    return;
+                                }
+                            }
+                        });
+            } catch (RequestException e) {
+                // Couldn't connect to server
+                GWT.log("exception during login");
+            }
             //DEVEL ONLY FOR FAST LOGIN
             userService.loginUser(new UserDetail(username, password), new AsyncCallback<String>() {
                 @Override
---
+++ poptavka-web/src/main/java/cz/poptavka/sample/server/security/PoptavkaAuthenticationProvider.java
@@ -0,0 +1,57 @@
+package cz.poptavka.sample.server.security;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class PoptavkaAuthenticationProvider implements AuthenticationProvider {
+
+    private static final Log LOGGER = LogFactory.getLog(PoptavkaAuthenticationProvider.class);
+
+    private static Map<String, String> users = new HashMap<String, String>();
+
+    static {
+        users.put("fabrizio", "javacodegeeks");
+        users.put("justin", "javacodegeeks");
+        users.put("client@test.com", "kreslo");
+        users.put("supplier@test.com", "kreslo");
+    }
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        LOGGER.info("authentication");
+        String username = (String) authentication.getPrincipal();
+        String password = (String) authentication.getCredentials();
+
+        if (users.get(username) == null) {
+            throw new UsernameNotFoundException("User not found");
+        }
+
+        String storedPass = users.get(username);
+
+        if (!storedPass.equals(password)) {
+            throw new BadCredentialsException("Invalid password");
+        }
+
+        Authentication customAuthentication = new PoptavkaUserAuthentication("ROLE_USER", authentication);
+        customAuthentication.setAuthenticated(true);
+
+        return customAuthentication;
+
+    }
+
+    @Override
+    public boolean supports(Class<? extends Object> authentication) {
+        return UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication);
+    }
+
+}
+
