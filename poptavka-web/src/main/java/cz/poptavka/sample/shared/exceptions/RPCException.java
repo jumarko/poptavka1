43ade5b794bbba11ba545848e496434ad06c1880
kolki <kolki@61682b0c-635e-44ba-a85b-d27e7dc34134>
2012-04-15 18:56:17 +0000

    pridana cause do konstruktoru RPCException
    git-svn-id: https://svn.webgres.cz/svn/poptavka@975 61682b0c-635e-44ba-a85b-d27e7dc34134

--- poptavka-web/src/main/java/cz/poptavka/sample/shared/exceptions/RPCException.java
+++ poptavka-web/src/main/java/cz/poptavka/sample/shared/exceptions/RPCException.java
@@ -5,11 +5,22 @@ import java.io.Serializable;
 public class RPCException extends Exception implements Serializable {
     private String symbol;
 
+    private Throwable cause;
+
     public RPCException() {
     }
 
     public RPCException(String symbol) {
         this.symbol = symbol;
+    }
+
+    public RPCException(String symbol, Throwable cause) {
+        this.symbol = symbol;
+        this.cause = cause;
+    }
+
+    public Throwable getCause() {
+        return cause;
     }
 
     public String getSymbol() {
