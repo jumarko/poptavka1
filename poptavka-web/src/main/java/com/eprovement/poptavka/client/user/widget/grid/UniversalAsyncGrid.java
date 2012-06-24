a2a373aa40b944d81b8fcff72db46a9acf31cf35
martin <martin@61682b0c-635e-44ba-a85b-d27e7dc34134>
2012-06-24 07:37:32 +0000

    UniversalAsyncGrid - javadoc added
    git-svn-id: svn://46.137.95.166/poptavka@1041 61682b0c-635e-44ba-a85b-d27e7dc34134

--- poptavka-web/src/main/java/com/eprovement/poptavka/client/user/widget/grid/UniversalAsyncGrid.java
+++ poptavka-web/src/main/java/com/eprovement/poptavka/client/user/widget/grid/UniversalAsyncGrid.java
@@ -21,6 +21,10 @@ import java.util.Map;
 /**
  * Represent DataGrid with asynchronous data retrieving.
  *
+ * To use this class, eventBus of a witget that uses this class, must extend
+ * interface IEventBusData. Therefore getDataCount and getData methods must be
+ * implemented in module, which uses this class.
+ *
  * @author Martin
  * @param <T>
  */
@@ -30,15 +34,33 @@ public class UniversalAsyncGrid<T> extends DataGrid<T> {
     //                              INTERFACES                                *
     //*************************************************************************
     /**
-     * To use this class, eventBus which uses it must extend this interface.
+     * Provides interfaces for methods retrieving count and data. Each eventBus
+     * of module using this class must extend this interface.
      */
     public interface IEventBusData {
 
+        /**
+         * Counts data that satisfy filtering criteria represented by given
+         * SearchModuleDataHolder.
+         *
+         * @param grid
+         * @param searchDataHolder - define filtering criteria
+         */
         void getDataCount(UniversalAsyncGrid grid, SearchModuleDataHolder searchDataHolder);
 
+        /**
+         * Gets data that satisfy filtering criteria represented by given
+         * SearchModuleDataHolder. Supports pagination and ordering.
+         *
+         * @param start - pagination - start
+         * @param maxResult - pagination - page size
+         * @param searchDataHolder - define filtering criteria
+         * @param orderColumns - define ordering
+         */
         void getData(int start, int maxResult,
                 SearchModuleDataHolder searchDataHolder, Map<String, OrderType> orderColumns);
     }
+
     /**
      * Get a cell value from a record.
      *
@@ -48,28 +70,65 @@ public class UniversalAsyncGrid<T> extends DataGrid<T> {
 
         C getValue(Object object);
     }
-
     //*************************************************************************/
     //                          ATTRIBUTES                                    */
     //*************************************************************************/
-    // Data Provider
+    /**
+     * Asynchronous Data Provider. When all data count is known, asynchronous
+     * data provider is created {@link #createAsyncDataProvider(final int resultCount)}.
+     * Asks for new data when user choose different page.
+     */
     private AsyncDataProvider<T> dataProvider = null;
     private int start = 0;
-    // Sorting
+    /**
+     * Asynchronous sorting handler. Provides handler for sorting data where
+     * asynchronous data provider is used. When asynchronous data provider is
+     * created, creates asynchronous sorting handler too. See {@link #createAsyncSortHandler()}.
+     */
     private AsyncHandler sortHandler = null;
+    /**
+     * Holds information about sorting. Represent pairs <String, OrderType>
+     * where key is <b>column name</b> and value: <b>ASC, DESC</b>. Column Names
+     * are provided by
+     * (@link orderColumns) attribute.
+     */
     private Map<String, OrderType> orderColumns = new HashMap<String, OrderType>();
+    /**
+     * Represents table column names. Each widget using this class
+     * (UniversalAsyncGrid) must define column names when creating
+     * UniversalAsyncGrid. Column names are then used as key in orderColumns
+     * attribute.
+     */
     private List<String> gridColumns = null;
-    // Others
+    /**
+     * Stores eventBus of module that uses this class. Stored when
+     * (@link getDataCount(IEventBusData eventBus, SearchModuleDataHolder searchDataHolder)
+     * method is called.
+     */
     private IEventBusData eventBus = null;
+    /**
+     * Holds filtering criteria. See class (@link SearchModuleDataHolder).
+     */
     private SearchModuleDataHolder searchDataHolder = null;
     private final SingleSelectionModel<T> selectionModel = new SingleSelectionModel<T>();
 
+    /**
+     * Constructor of UniversalAsyncGrid.
+     *
+     * @param gridColumns - define table column names
+     */
     public UniversalAsyncGrid(List<String> gridColumns) {
         super();
         setSelectionModel(selectionModel);
         this.gridColumns = gridColumns;
     }
 
+    /**
+     * Constructor of UniversalAsyncGrid.
+     *
+     * @param keyProvider - define key provider for selection model
+     * @param gridColumns - define table column names
+     */
     public UniversalAsyncGrid(ProvidesKey<T> keyProvider, List<String> gridColumns) {
         super(keyProvider);
         setSelectionModel(selectionModel);
@@ -77,12 +136,12 @@ public class UniversalAsyncGrid<T> extends DataGrid<T> {
     }
 
     /**
-     * Gets data count.
-     * Must be called first.
+     * Gets data count. Whole retrieving proces starts here, therefore must be
+     * called to fire it.
      *
-     * @param eventBus - define eventBus to handle <b>getDataCount</b> and <b>getData</b> methods
+     * @param eventBus - define eventBus to handle <b>getDataCount</b> and
+     * <b>getData</b> methods
      * @param searchDataHolder - define search criteria if any
-     * @param gridColumns - define column names of table
      */
     public void getDataCount(IEventBusData eventBus, SearchModuleDataHolder searchDataHolder) {
         this.eventBus = eventBus;
@@ -92,8 +151,9 @@ public class UniversalAsyncGrid<T> extends DataGrid<T> {
     }
 
     /**
-     * Creates asynchronous data provider for table.
-     * Must be called when count of all data is known.
+     * Creates asynchronous data provider for a table. Must be called when count
+     * of all data is known. Method onRangeChanged it called when user choose
+     * different page.
      *
      * @param resultCount - count of all data
      */
@@ -109,6 +169,7 @@ public class UniversalAsyncGrid<T> extends DataGrid<T> {
 
                 orderColumns.clear();
                 orderColumns.put(gridColumns.get(0), OrderType.DESC);
+                //Aks for new data
                 eventBus.getData(start, start + length, searchDataHolder, orderColumns);
             }
         };
@@ -118,7 +179,9 @@ public class UniversalAsyncGrid<T> extends DataGrid<T> {
     }
 
     /**
-     * Creates aynchronous handler for sorting.
+     * Creates aynchronous handler for sorting. Its called when asynchronous
+     * data provider is created. See (@link createAsyncDataProvider(final int resultCount)).
+     * Method onColumnSort it called when user choose different column to sort.
      */
     private void createAsyncSortHandler() {
         //Moze byt hned na zaciatku? Ak ano , tak potom aj asynchdataprovider by mohol nie?
@@ -147,10 +210,13 @@ public class UniversalAsyncGrid<T> extends DataGrid<T> {
     // ADDITIONAL "OVERRIDE" METHODS
     //*************************************************************************/
     /**
-     * Add a column with a header.
+     * Add a column with a header. When creating table in module using this
+     * class, it uses this method to create table columns.
      *
      * @param <C> - the cell type
      * @param cell - the cell used to render the column
+     * @param sort - define whether column will be allowed to be sorted
+     * @param width - define column width
      * @param headerText - the header string
      * @param getter - the value getter for the cell
      */
@@ -171,6 +237,12 @@ public class UniversalAsyncGrid<T> extends DataGrid<T> {
         return column;
     }
 
+    /**
+     * Updates data provider content. When new data for are available, call this
+     * method for display it.
+     *
+     * @param list
+     */
     public void updateRowData(List<T> list) {
         dataProvider.updateRowData(start, list);
         flush();
@@ -180,24 +252,49 @@ public class UniversalAsyncGrid<T> extends DataGrid<T> {
     // ***********************************************************************
     // Getter metods
     // ***********************************************************************
+    /**
+     * Gets asynchronous data provider provided by UniversalAsyncGrid class.
+     *
+     * @return data provider
+     */
     public AsyncDataProvider<T> getDataProvider() {
         return dataProvider;
     }
 
+    /**
+     * Gets asynchronous sorting handler provided by UniversalAsyncGrid class.
+     *
+     * @return
+     */
     public AsyncHandler getSortHandler() {
         return sortHandler;
     }
 
+    /**
+     * Gets single selection model provided by UniversalAsyncGrid class.
+     *
+     * @return
+     */
     @Override
     public SingleSelectionModel<T> getSelectionModel() {
         return selectionModel;
     }
 
+    /**
+     * Displays message when no data are available for displaying. Customize
+     * method content for user delight. Calls when instance of this class is
+     * created.
+     */
     public void displayEmptyTable() {
         this.setEmptyTableWidget(new HTML("<div style=\"text-align: center;\">"
                 + Storage.MSGS.emptyTable() + "</div>"));
     }
 
+    /**
+     * Gets start index of pagination.
+     *
+     * @return start index
+     */
     public int getStart() {
         return start;
     }
