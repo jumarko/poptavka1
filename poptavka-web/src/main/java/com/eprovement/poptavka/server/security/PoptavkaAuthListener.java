6c5bbd1b7b6c61c860111bee57bd1ff8f72b6151
praso <praso@61682b0c-635e-44ba-a85b-d27e7dc34134>
2012-07-26 23:51:50 +0000

    Tracking of auth events added
    git-svn-id: svn://46.137.95.166/poptavka@1126 61682b0c-635e-44ba-a85b-d27e7dc34134

--- poptavka-web/src/main/java/com/eprovement/poptavka/server/security/PoptavkaAuthListener.java
+++ poptavka-web/src/main/java/com/eprovement/poptavka/server/security/PoptavkaAuthListener.java
@@ -2,11 +2,19 @@ package com.eprovement.poptavka.server.security;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.context.ApplicationEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.security.authentication.event.AbstractAuthenticationEvent;
 import org.springframework.security.authentication.event.AbstractAuthenticationFailureEvent;
+import org.springframework.security.authentication.event.AuthenticationFailureBadCredentialsEvent;
+import org.springframework.security.authentication.event.InteractiveAuthenticationSuccessEvent;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
+import org.springframework.security.web.session.HttpSessionDestroyedEvent;
+
 /**
  * This class represent listener to authentication event
+ *
  * @author kolkar
  *
  */
@@ -31,6 +39,48 @@ public class PoptavkaAuthListener implements ApplicationListener<AbstractAuthent
 
         LOGGER.warn(builder.toString());
 
-    }
+        // Advanced tracking of login, logout and session
+        //Session ends
+        ApplicationEvent appEvent = (ApplicationEvent) event;
+        if (appEvent instanceof HttpSessionDestroyedEvent) {
+            String username = "";
+            String remoteAddress = null;
+            HttpSessionDestroyedEvent logoutEvent = (HttpSessionDestroyedEvent) appEvent;
+            SecurityContext securityContext = (SecurityContext) logoutEvent.
+                    getSession().getAttribute("SPRING_SECURITY_CONTEXT");
+            if (securityContext != null) {
+                username = (String) logoutEvent.getSession().getAttribute("SPRING_SECURITY_LAST_USERNAME");
+                WebAuthenticationDetails webAuthenticationDetails = (WebAuthenticationDetails) securityContext.
+                        getAuthentication().getDetails();
+                if (webAuthenticationDetails != null) {
+                    remoteAddress = webAuthenticationDetails.getRemoteAddress();
+                }
+                //Log action
+                LOGGER.warn("applicationEvent is HttpSessionDestroyedEvent, username="
+                        + username + ", remoteAddress=" + remoteAddress);
+            }
+            return;
+        }
+        //Success
+        if (appEvent instanceof InteractiveAuthenticationSuccessEvent) {
+            InteractiveAuthenticationSuccessEvent successEvent = (InteractiveAuthenticationSuccessEvent) appEvent;
+            WebAuthenticationDetails webAuthenticationDetails = (WebAuthenticationDetails) successEvent.
+                    getAuthentication().getDetails();
+            String username = successEvent.getAuthentication().getName();
+            //Log action
+            LOGGER.warn("applicationEvent is InteractiveAuthenticationSuccessEvent: username=" + username);
+            return;
+        }
+        //Bad credentials
+        if (appEvent instanceof AuthenticationFailureBadCredentialsEvent) {
+            AuthenticationFailureBadCredentialsEvent failureEvent = (AuthenticationFailureBadCredentialsEvent) appEvent;
+            WebAuthenticationDetails webAuthenticationDetails = (WebAuthenticationDetails) failureEvent.
+                    getAuthentication().getDetails();
+            String username = failureEvent.getAuthentication().getName();
+            //Log action
+            LOGGER.warn("applicationEvent is AuthenticationFailureBadCredentialsEvent: username=" + username);
+            return;
+        }
 
+    }
 }
