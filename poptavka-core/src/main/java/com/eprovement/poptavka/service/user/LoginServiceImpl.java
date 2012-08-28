9bac09675d8ba6e1395db7d0f9b63139156b8950
praso <praso@61682b0c-635e-44ba-a85b-d27e7dc34134>
2012-08-28 16:20:10 +0000

    LoginServiceImpl getLoggedUser retrieves user from Authentication object
    git-svn-id: svn://46.137.95.166/poptavka@1212 61682b0c-635e-44ba-a85b-d27e7dc34134

--- poptavka-core/src/main/java/com/eprovement/poptavka/service/user/LoginServiceImpl.java
+++ poptavka-core/src/main/java/com/eprovement/poptavka/service/user/LoginServiceImpl.java
@@ -14,25 +14,21 @@ import com.eprovement.poptavka.exception.IncorrectPasswordException;
 import com.eprovement.poptavka.exception.LoginException;
 import com.eprovement.poptavka.service.GeneralService;
 import org.jasypt.exceptions.EncryptionOperationNotPossibleException;
-
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 /**
  *
  * Basic imlementation of {@link LoginService}.
  *
- * @author Juraj Martinka
- *         Date: 30.3.2012
+ * @author Juraj Martinka Date: 30.3.2012
  */
 public class LoginServiceImpl implements LoginService {
 
     /**
      * TODO: remove this.
      */
-    private static final long LOGGED_USER_ID = 1L;
-
     private static final Logger LOGGER = LoggerFactory.getLogger(LoginServiceImpl.class);
-
-
     private final GeneralService generalService;
     private Encryptor encryptor;
 
@@ -44,21 +40,38 @@ public class LoginServiceImpl implements LoginService {
     }
 
     /**
-     * TODO: replace constant user id with ID of real logged user.
-     * @return
+     * Method returns logged user from Spring Security Authentication object.
+     * User has been successfully authenticated before and here he is retrieved
+     * from Authentication object.
+     *
+     * TODO: implement test.
+     *
+     * @return user - user retrieved from Spring Security Authentication object
      */
     @Override
     @Transactional(readOnly = true)
     public User getLoggedUser() {
-        final User loggedUser = new User();
-        loggedUser.setId(LOGGED_USER_ID);
-        return loggedUser;
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null) {
+            LOGGER.debug("Not logged in. Authentication object is null");
+            return null;
+        } else {
+            String username = (String) authentication.getPrincipal(); // username equals email
+            final Search searchByEmail = new Search(User.class);
+            searchByEmail.addFilterEqual("email", username.trim());
+
+            // a user must exist since he has already been authenticated before
+            final User user = (User) this.generalService.searchUnique(searchByEmail);
+            return user;
+        }
     }
 
-
     /**
-     *  {@inheritDoc}
-     *  @throws IllegalArgumentException if {@code email} or {@code password} is empty
+     * {@inheritDoc}
+     *
+     * @throws IllegalArgumentException if {@code email} or {@code password} is
+     * empty
      */
     @Override
     @Transactional(readOnly = true)
@@ -77,7 +90,7 @@ public class LoginServiceImpl implements LoginService {
         }
 
         try {
-            if (! encryptor.matches(password, user.getPassword())) {
+            if (!encryptor.matches(password, user.getPassword())) {
                 throw new IncorrectPasswordException("For user with email=" + email);
             }
         } catch (EncryptionOperationNotPossibleException eonpe) {
