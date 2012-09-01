26167ce95c00a064a71f2faf3ccc4bc29d81e56b
martin <martin@61682b0c-635e-44ba-a85b-d27e7dc34134>
2012-09-01 13:34:20 +0000

    BUGFIX: Demands module packages and RPC removed
    git-svn-id: svn://46.137.95.166/poptavka@1221 61682b0c-635e-44ba-a85b-d27e7dc34134

--- poptavka-web/src/main/java/com/eprovement/poptavka/client/user/demands/DemandModule.java
+++
@@ -1,9 +0,0 @@
-package com.eprovement.poptavka.client.user.demands;
-
-import com.mvp4g.client.Mvp4gModule;
-import com.mvp4g.client.annotation.module.HistoryName;
-
-@HistoryName("userDemands")
-public interface DemandModule extends Mvp4gModule {
-
-}
