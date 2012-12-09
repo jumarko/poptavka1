c3b3dbc3699ef2af54e2de334b87c6db55dd6cd1
jaro <jaro@61682b0c-635e-44ba-a85b-d27e7dc34134>
2012-12-09 21:10:12 +0000

    UI changes:
    - header layout
    - login styles + image for new messages update
    - very low css refactoring
    
    git-svn-id: svn://46.137.95.166/poptavka@1392 61682b0c-635e-44ba-a85b-d27e7dc34134

--- poptavka-web/src/main/java/com/eprovement/poptavka/client/resources/LayoutResource.java
+++ poptavka-web/src/main/java/com/eprovement/poptavka/client/resources/LayoutResource.java
@@ -14,74 +14,109 @@ public interface LayoutResource extends CssResource {
     /** MainView.class **/
     @ClassName("layout-public-main")
     String layoutPublic();
+
     @ClassName("layout-user-main")
     String layoutUser();
+
     @ClassName("header-container")
     String headerContainer();
+
     @ClassName("header-content")
     String headerContent();
+
     @ClassName("footer-container")
     String footerContainer();
+
     @ClassName("body-container")
     String bodyContainer();
+
     @ClassName("body-full-container")
     String bodyFullContainer();
+
     @ClassName("body-background")
     String bodyBackground();
+
     @ClassName("body-navbar")
     String bodyNavbar();
+
     @ClassName("detail-info")
     String detailInfo();
-    @ClassName("login-area")
-    String loginArea();
+
+    /* login styles */
+    @ClassName("login")
+    String login();
+
+    @ClassName("login-bar")
+    String loginBar();
+
     @ClassName("login-button")
     String loginButton();
+
     @ClassName("logout-button")
     String logoutButton();
+
     @ClassName("logout-menu-bar")
     String logoutMenuBar();
+
+    @ClassName("help-button")
+    String helpButton();
+
     @ClassName("footer-toggle")
     String footerToggle();
+
     @ClassName("home-menu")
     String homeMenu();
+
     @ClassName("search-bar")
     String searchBar();
+
     @ClassName("search-view")
     String searchView();
+
     @ClassName("selected")
     String selected();
 
     /** UserView.class **/
     @ClassName("full-size-panel")
     String fullSize();
+
     @ClassName("tab-layout-container")
     String tabLayoutContainer();
+
     @ClassName("tab-layout-panel")
     String tabLayoutPanel();
+
     @ClassName("demand-tab-content")
     String demandTabContent();
+
     @ClassName("demand-tab-content-header")
     String demandTabContentHeader();
+
     @ClassName("pager")
     String pager();
+
     @ClassName("submenu-header")
     String submenuHeader();
+
     @ClassName("submenu-list")
     String submenuList();
 
     /** DemandCreation.class **/
     @ClassName("stack-layout")
     String stackLayout();
+
     @ClassName("stack-layout-panel-header")
     String stackLayoutPanelHeader();
+
     @ClassName("create-tab-layout")
     String createTabLayout();
+
     @ClassName("create-tab-layout-header")
     String createTabLayoutHeader();
+
     @ClassName("create-tab-layout-selected")
     String createTabLayoutSelected();
 
     /** DELETE AFTER THIS LINE **/
-
 
 }
