4bf99cba575447f2091a7f9224551441ef5e8ff6
martin <martin@61682b0c-635e-44ba-a85b-d27e7dc34134>
2013-01-25 19:46:10 +0000

    Home module removed.
    git-svn-id: svn://46.137.95.166/poptavka@1568 61682b0c-635e-44ba-a85b-d27e7dc34134

--- poptavka-web/src/main/java/com/eprovement/poptavka/client/home/HomeModule.java
+++
@@ -1,9 +0,0 @@
-package com.eprovement.poptavka.client.home;
-
-import com.mvp4g.client.Mvp4gModule;
-import com.mvp4g.client.annotation.module.HistoryName;
-
-@HistoryName("public")
-public interface HomeModule extends Mvp4gModule {
-
-}
