55b8ca65cf230147c67fc7f9c758b76ca997e2a0
Juraj Martinka <jumarko@gmail.com>
2013-04-16 19:36:33 +0200

    FEATURE: #37 - enabled flag added to the DomainObject
    Only "enabled = 1 OR enabled IS NULL" is a valid condition because enabled = "some string" is always evaluated as a true.

--- poptavka-core/src/main/java/com/eprovement/poptavka/domain/common/DomainObject.java
+++ poptavka-core/src/main/java/com/eprovement/poptavka/domain/common/DomainObject.java
@@ -3,6 +3,7 @@ package com.eprovement.poptavka.domain.common;
 import org.apache.lucene.analysis.cz.CzechAnalyzer;
 import org.hibernate.search.annotations.Analyzer;
 
+import javax.persistence.Column;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -24,6 +25,10 @@ import java.io.Serializable;
  */
 @Analyzer(impl = CzechAnalyzer.class)
 @MappedSuperclass
+// filter all disabled objects - both '1' and NULL are considered as being enabled, object has to be disabled explicitly
+@FilterDef(name = DomainObject.ENABLED_FILTER_NAME,
+        defaultCondition = "(enabled = '1' OR enabled IS NULL)")
+@Filter(name = DomainObject.ENABLED_FILTER_NAME)
 public abstract class DomainObject implements Serializable {
 
     /** Id of the entity. */
@@ -31,6 +36,17 @@ public abstract class DomainObject implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    /**
+     * Determines whether this entity should be considered at all or not.
+     * By default each entity is enabled unless explicitly made disabled.
+     * Enabled flag is a less offensive substitution for deleting from database.
+     *
+     * This attribute has associated "columnDefinition" BIT which serves as a workaround, see:
+     * <a href="http://stackoverflow.com/questions/8667965/found-bit-expected-boolean-after-hibernate-4-upgrade">
+     *     found-bit-expected-boolean-after-hibernate-4-upgrade</a>
+     */
+    @Column(columnDefinition = "BIT")
+    private Boolean enabled = true;
 
     public Long getId() {
         return id;
@@ -41,6 +57,26 @@ public abstract class DomainObject implements Serializable {
             throw new NullPointerException("id");
         }
         this.id = id;
+    }
+
+    /**
+     * By default all domain objects should be enabled so this method returns true even if {@code enabled} is unset.
+      * @return true if {@code enabled} flag is true or null, false otherwise
+     */
+    public Boolean isEnabled() {
+        if (enabled == null) {
+            // always consider unset "enabled" as true value
+            return true;
+        }
+        return enabled;
+    }
+
+    /**
+     * Sets whether this domain object is enabled or not.
+     * @param enabled whether domain object should be enabled or not
+     */
+    public void setEnabled(Boolean enabled) {
+        this.enabled = enabled;
     }
 
     @Override
