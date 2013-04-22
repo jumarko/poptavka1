75eee64d1f8ae90ba840b0227f04f2d2e89eacf1
Martin Slavkovsky <xslavkov@gmail.com>
2013-04-22 12:38:35 +0200

    Old styles removed

--- poptavka-web/src/main/java/com/eprovement/poptavka/client/user/widget/grid/cell/MessageCell.java
+++ poptavka-web/src/main/java/com/eprovement/poptavka/client/user/widget/grid/cell/MessageCell.java
@@ -31,7 +31,7 @@ public class MessageCell extends AbstractCell<MessageDetail> {
 
     interface MyUiRenderer extends UiRenderer {
 
-        void render(SafeHtmlBuilder sb, String cssBall, String cssColor, String sender, String date, String body);
+        void render(SafeHtmlBuilder sb, String cssColor, String sender, String date, String body);
 
         void onBrowserEvent(MessageCell o, NativeEvent e, Element p, MessageDetail n);
 
@@ -55,16 +55,13 @@ public class MessageCell extends AbstractCell<MessageDetail> {
     /**************************************************************************/
     @Override
     public void render(Context context, MessageDetail value, SafeHtmlBuilder sb) {
-        String cssBall;
         String cssColor;
         if (Storage.getUser().getUserId() == value.getSenderId()) {
-            cssBall = Storage.RSCS.detailViews().conversationDetailRed();
             cssColor = Storage.RSCS.detailViews().conversationDetailHeaderRed();
         } else {
-            cssBall = Storage.RSCS.detailViews().conversationDetailGreen();
             cssColor = Storage.RSCS.detailViews().conversationDetailHeaderGreen();
         }
-        renderer.render(sb, cssBall, cssColor, value.getSender(),
+        renderer.render(sb, cssColor, value.getSender(),
                 getSentText(value.getSent()), getBodyText(value.getBody()));
     }
 
