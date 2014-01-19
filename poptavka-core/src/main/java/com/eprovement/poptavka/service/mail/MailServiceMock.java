267cea0d5a1eef2f2d2396ba778f843076d02f04
Juraj Martinka <juraj.martinka@gooddata.com>
2014-01-19 08:38:47 +0100

    MailServiceMock fixes required for WelcomeMessageSenderIntegrationTest

--- poptavka-core/src/main/java/com/eprovement/poptavka/service/mail/MailServiceMock.java
+++ poptavka-core/src/main/java/com/eprovement/poptavka/service/mail/MailServiceMock.java
@@ -3,24 +3,51 @@
  */
 package com.eprovement.poptavka.service.mail;
 
+import static java.util.Arrays.asList;
+import static java.util.Collections.unmodifiableList;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import javax.mail.internet.MimeMessage;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
- * Dummy implementation of {@link com.eprovement.poptavka.service.mail.MailService} that doesn't do anything.
+ * Dummy implementation of {@link com.eprovement.poptavka.service.mail.MailService} that doesn't really send messages
+ * but only remember them.
+ * The sent messages can be retrieved via {@link #getSentMimeMessages()}  or {@link #getSentSimpleMailMessages()}.
  */
 public class MailServiceMock implements MailService {
 
-    @Override
-    public void send(MimeMessage... message) { }
+    private final List<MimeMessage> sentMimeMessages = new ArrayList<>();
+    private final List<SimpleMailMessage> sentSimpleMailMessages = new ArrayList<>();
 
     @Override
-    public void send(SimpleMailMessage... message) { }
+    public void send(MimeMessage... message) {
+        sentMimeMessages.addAll(asList(message));
+    }
 
     @Override
-    public void sendAsync(MimeMessage... message) { }
+    public void send(SimpleMailMessage... message) {
+        sentSimpleMailMessages.addAll(asList(message));
+    }
 
     @Override
-    public void sendAsync(SimpleMailMessage... message) { }
+    public void sendAsync(MimeMessage... message) {
+        sentMimeMessages.addAll(asList(message));
+    }
+
+    @Override
+    public void sendAsync(SimpleMailMessage... message) {
+        sentSimpleMailMessages.addAll(asList(message));
+    }
+
+
+    public List<MimeMessage> getSentMimeMessages() {
+        return unmodifiableList(sentMimeMessages);
+    }
+
+    public List<SimpleMailMessage> getSentSimpleMailMessages() {
+        return unmodifiableList(sentSimpleMailMessages);
+    }
 }
