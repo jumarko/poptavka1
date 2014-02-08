f40f852fba9052b5096cc4e48b9a9f812cacf139
Juraj Martinka <juraj.martinka@gooddata.com>
2014-02-08 16:02:26 +0100

    LocalityZipcode: resources fix - always use LocalityDeserializer and don't duplicate code

--- poptavka-rest-api/src/main/java/com/eprovement/poptavka/rest/common/serializer/LocalityDeserializer.java
+++ poptavka-rest-api/src/main/java/com/eprovement/poptavka/rest/common/serializer/LocalityDeserializer.java
@@ -1,11 +1,12 @@
 package com.eprovement.poptavka.rest.common.serializer;
 
+import static org.apache.commons.lang.StringUtils.isNotEmpty;
 import static org.apache.commons.lang.Validate.notNull;
 
 import com.eprovement.poptavka.domain.address.Locality;
 import com.eprovement.poptavka.rest.common.dto.LocalityDto;
 import com.eprovement.poptavka.service.address.LocalityService;
-import org.apache.commons.lang.StringUtils;
+import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -13,7 +14,7 @@ import java.util.Collection;
 import java.util.List;
 
 @Service
-public class LocalityDeserializer {
+public class LocalityDeserializer implements Converter<LocalityDto, Locality> {
 
     private final LocalityService localityService;
 
@@ -26,23 +27,26 @@ public class LocalityDeserializer {
     public List<Locality> convertLocalities(Collection<LocalityDto> localityDtos) {
         final List<Locality> localities = new ArrayList<>();
         for (LocalityDto localityDto : localityDtos) {
-            final Locality locality;
-            // try to find locality - ID always takes a precedence
-            if (localityDto.getId() != null) {
-                locality = localityService.getLocality(localityDto.getId());
-            } else if (StringUtils.isNotEmpty(localityDto.getCity())) {
-                locality = localityService.findCityByName(localityDto.getRegion(), localityDto.getDistrict(),
-                        localityDto.getCity());
-            } else if (StringUtils.isNotEmpty(localityDto.getDistrict())) {
-                locality = localityService.findDistrictByName(localityDto.getRegion(), localityDto.getDistrict());
-            } else {
-                locality = localityService.findRegion(localityDto.getRegion());
-            }
-            notNull(locality, "No locality has been found for dto=" + localityDto);
-            localities.add(locality);
-
+            localities.add(convert(localityDto));
         }
         return localities;
     }
 
+    @Override
+    public Locality convert(LocalityDto localityDto) {
+        // try to find locality - ID always takes a precedence
+        final Locality locality;
+        if (localityDto.getId() != null) {
+            locality = localityService.getLocality(localityDto.getId());
+        } else if (isNotEmpty(localityDto.getCity())) {
+            locality = localityService.findCityByName(localityDto.getRegion(), localityDto.getDistrict(),
+                    localityDto.getCity());
+        } else if (isNotEmpty(localityDto.getDistrict())) {
+            locality = localityService.findDistrictByName(localityDto.getRegion(), localityDto.getDistrict());
+        } else {
+            locality = localityService.findRegion(localityDto.getRegion());
+        }
+        notNull(locality, "No locality has been found for dto=" + localityDto);
+        return locality;
+    }
 }
