b0b8b76192c7a2b9f55c99483667969759d61a81
Juraj Martinka <jumarko@gmail.com>
2019-06-17 19:04:42 +0200

    more codeowners stuff - directory with dot in name

---
+++ dir.space/file.java
@@ -0,0 +1,104 @@
+/** This is for code owners */
+public static void main (String[] args) {
+  System.out.println("Ahoj");
+}
+
+public static void unused() {
+  System.out.println("I'm alone!");
+  System.out.println("I'm alone!");
+  System.out.println("I'm alone!");
+  System.out.println("I'm alone!");
+  System.out.println("I'm alone!");
+  System.out.println("I'm alone!");
+  System.out.println("I'm alone!");
+  System.out.println("I'm alone!");
+  System.out.println("I'm alone!");
+  System.out.println("I'm alone!");
+  System.out.println("I'm alone!");
+  System.out.println("I'm alone!");
+  System.out.println("I'm alone!");
+  System.out.println("I'm alone!");
+  System.out.println("I'm alone!");
+  System.out.println("I'm alone!");
+  System.out.println("I'm alone!");
+  System.out.println("I'm alone!");
+  System.out.println("I'm alone!");
+  System.out.println("I'm alone!");
+  System.out.println("I'm alone!");
+  System.out.println("I'm alone!");
+  System.out.println("I'm alone!");
+  System.out.println("I'm alone!");
+  System.out.println("I'm alone!");
+  System.out.println("I'm alone!");
+  System.out.println("I'm alone!");
+  System.out.println("I'm alone!");
+  System.out.println("I'm alone!");
+}
+
+public static int age() {
+  if (age > 10) {
+    return age;
+  }
+
+  if (age > 10) {
+    return age;
+  }
+
+  if (age > 10) {
+    return age;
+  }
+  if (age > 10) {
+    return age;
+  }
+  if (age > 10) {
+    return age;
+  }
+  if (age > 10) {
+    return age;
+  }
+  if (age > 10) {
+    return age;
+  }
+  if (age > 10) {
+    return age;
+  }
+  if (age > 10) {
+    return age;
+  }
+  if (age > 10) {
+    return age;
+  }
+  if (age > 10) {
+    return age;
+  }
+  if (age > 10) {
+    return age;
+  }
+  if (age > 10) {
+    return age;
+  }
+  if (age > 10) {
+    return age;
+  }
+  if (age > 10) {
+    return age;
+  }
+  if (age > 10) {
+    return age;
+  }
+  if (age > 10) {
+    return age;
+  }
+  if (age > 10) {
+    return age;
+  }
+  if (age > 10) {
+    return age;
+  }
+  if (age > 10) {
+    return age;
+  }
+  if (age > 10) {
+    return age;
+  }
+}
