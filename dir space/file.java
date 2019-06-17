2d0f805eee557cf8b20f43f8853be0f86563f98c
Juraj Martinka <jumarko@gmail.com>
2019-06-17 17:34:48 +0200

    dir\ space change

--- dir space/file.java
+++ dir space/file.java
@@ -2,3 +2,71 @@
 public static void main (String[] args) {
   System.out.println("Ahoj");
 }
+
+public static int age() {
+  if (age > 10) {
+    return age;
+  }
+
+  if (age > 10) {
+    return age;
+  }
+
+  if (age > 10) {
+    return age;
+  }
+  if (age > 10) {
+    return age;
+  }
+  if (age > 10) {
+    return age;
+  }
+  if (age > 10) {
+    return age;
+  }
+  if (age > 10) {
+    return age;
+  }
+  if (age > 10) {
+    return age;
+  }
+  if (age > 10) {
+    return age;
+  }
+  if (age > 10) {
+    return age;
+  }
+  if (age > 10) {
+    return age;
+  }
+  if (age > 10) {
+    return age;
+  }
+  if (age > 10) {
+    return age;
+  }
+  if (age > 10) {
+    return age;
+  }
+  if (age > 10) {
+    return age;
+  }
+  if (age > 10) {
+    return age;
+  }
+  if (age > 10) {
+    return age;
+  }
+  if (age > 10) {
+    return age;
+  }
+  if (age > 10) {
+    return age;
+  }
+  if (age > 10) {
+    return age;
+  }
+  if (age > 10) {
+    return age;
+  }
+}
